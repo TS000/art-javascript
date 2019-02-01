c9546507692125e12ca848793af7404b81ebc899
TS000 <tylersnell@me.com>
2019-02-01 15:44:46 -0700

    red circles

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
-const { lerp } = require('canvas-sketch-util/math')
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 2048, 2048 ]
@@ -9,32 +10,42 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 40;
     for (let x = 0; x < count; x++){
       for (let y = 0; y < count; y++){
-        const u = count <= 1 ? .5 : x / (count - 1);
-        const v = count <= 1 ? .5 : y / (count - 1);
-        points.push([ u, v ]);
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.01),
+          position: [ u, v ]
+        });
       }
     }
     return points;
   };
 
-  const points = createGrid();
+  random.setSeed('bazoka');
+  const points = createGrid().filter(() => random.value() > 0.5)
+  const margin = 400;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([ u, v ]) => {
-      const x = u * width;
-      const y = v * height;
+    points.forEach(data => {
+      const { position, radius } = data;
+
+      const [ u, v ] = position;
+
+
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, width - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 100, 0, Math.PI *2, false);
-      context.strokeStyle = 'rebeccapurple';
-      context.lineWidth = 20;
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI *2, false);
+      context.lineWidth = 10;
+      context.fillStyle = 'red';
+      context.fill();
     })
   };
 };
