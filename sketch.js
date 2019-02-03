f508fd4a01e06fed64895e5ba20d2094d055a7f0
Tyler S <ghost@Tylers-Mac-mini.local>
2019-02-03 16:57:24 -0700

    unicode

--- sketch.js
+++ sketch.js
@@ -3,63 +3,66 @@ const { lerp } = require('canvas-sketch-util/math');
 const palettes = require('nice-color-palettes/1000.json');
 const random = require('canvas-sketch-util/random');
 
-let palette = random.pick(palettes);
-
-palette = random.shuffle(palette);
-palette = palette.slice(0, random.rangeFloor(2, palette.length + 1));
-
-const background = palette.shift();
+random.setSeed(random.getRandomSeed())
 
 const settings = {
+  suffix: random.getSeed(),
   dimensions: [ 2048, 2048 ]
 };
 
-const sketch = () => {
-  const count = 30;
+  const sketch = () => {
+  const colorCount = random.rangeFloor(2, 6);
+  const palette = random.shuffle(random.pick(palettes))
+    .slice(0, colorCount);
 
   const createGrid = () => {
     const points = [];
+    const count = 40;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
-        const u = x / (count - 1);
-        const v = y / (count - 1);
-        const position = [ u, v ];
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : x / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.2;
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(30 + 20 * random.gaussian()),
-          position
+          radius,
+          rotation: random.noise2D(u, v) * 5,
+          position: [ u, v ]
         });
       }
     }
     return points;
   };
 
-  let points = createGrid().filter(() => {
-    return Math.random() > 0.75;
+const points = createGrid().filter(() => random.value() > 0.5);
+const margin = 400;
+
+return ({ context, width, height }) => {
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, width, height);
+
+  points.forEach(data => {
+    const {
+      position,
+      radius,
+      color,
+      rotation
+    } = data;
+
+    const [ u, v ] = position;
+
+    const x = lerp(margin, width - margin, u);
+    const y = lerp(margin, height - margin, v);
+    
+    context.save();
+    context.fillStyle = color;
+    context.font = `${radius * width}px "Helvetica"`;
+    context.translate(x, y);
+    context.rotate(rotation);
+    context.fillText( 'w', 0, 0);
+
+    context.restore();
   });
-
-  points = random.shuffle(points);
-
-  return ({ context, width, height }) => {
-    const margin = width * 0.175;
-
-    context.fillStyle = background;
-    context.fillRect(0, 0, width, height);
-
-    points.forEach(data => {
-      const {
-        position,
-        radius,
-        color
-      } = data;
-      const x = lerp(margin, width - margin, position[0]);
-      const y = lerp(margin, height - margin, position[1]);
-
-      context.beginPath();
-      context.arc(x, y, radius, 0, Math.PI * 2);
-      context.fillStyle = color;
-      context.fill();
-    });
   };
 };
 
